eab7c7d0ee6bba035cc26a78fb04c0d2010ab335
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-04-19 15:45:09 -0300

    feat: token validator middleware

---
+++ src/middlewares/token.middleware.js
@@ -0,0 +1,17 @@
+import { validate } from "uuid";
+
+async function validateToken(req, res, next) {
+  const { authorization } = req.headers;
+  if (!authorization || typeof authorization !== "string") {
+    return res.status(401).send("no token found");
+  }
+  const token = authorization.replace("Bearer ", "");
+  if (!validate(token)) {
+    return res.status(401).send("token is invalid");
+  }
+  next();
+}
+
+const tokenMiddlewares = { validateToken };
+
+export default tokenMiddlewares;
