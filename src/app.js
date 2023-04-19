ae60a71c1d960933c37bc38405827c97993045c7
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-04-19 15:26:21 -0300

    chore: dependencies

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 import express from "express";
-import { config } from "dotenv";
 import cors from "cors";
-config();
+import router from "./routes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(router);
 
-app.get("/", (req, res) => res.send("<h1>Hello World</h1>"));
-
-app.listen(process.env.PORT);
+const PORT = process.env.PORT ?? 5000;
+app.listen(PORT, () => console.log(`Running at ${PORT}`));
